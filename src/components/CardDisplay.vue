4de88e3dfc9076983ed2ff2f94508b3b1115d67d
fhiru <fhiruayang@gmail.com>
2024-01-29 23:30:10 +0700

    initiative

---
+++ src/components/CardDisplay.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card">
+        <div class="element">
+            <div class="image">
+                <img src="" alt="" class="img">
+            </div>
+            <div class="desc">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, inventore!
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 1034px;
+    height: 350px;
+    border-radius: 10px;
+    background-color: yellow;
+    padding: 20px;
+    box-sizing: border-box;
+}
+
+.element {
+    display: flex;
+    justify-content: space-between;
+}
+
+.image {
+    width: 300px;
+    background-color: blue;
+}
+
+.desc {
+    width: 550px;
+    border-radius: 4px;
+    background-color: pink;
+}
+</style>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,37 @@
+<script>
+import CardDisplay from './CardDisplay.vue';
+export default {
+    components: {
+        CardDisplay
+    }
+}
+</script>
+
+<template>
+    <div class="content">
+        <div class="top"></div>
+        <CardDisplay class="card" />
+    </div>
+</template>
+
+<style scoped>
+.content {
+    position: relative;
+    height: 100vh;
+    width: 100%;
+}
+
+.top {
+    height: 400px;
+    width: 100%;
+    background-color: #FDE2FF;
+}
+
+.card {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    background-color: transparent;
+    transform: translate(-50%, -50%);
+}
+</style>
